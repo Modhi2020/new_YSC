5cfbafdfd995745f6059e888acc3a580e829b484
Modhi Al-Obaidy <eng.modhi2020@gmail.com>
2022-12-18 12:22:22 +0300

    RPS commit

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,29 +13,15 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(PropertyPurposeSeeder::class);
-        $this->call(PropertyTypeSeeder::class);
+      
         $this->call(PostApprovedSeeder::class);
         $this->call(PostStatusSeeder::class);
 
         $this->call(AgreeSeeder::class);
-        $this->call(DenouncementTypeSeeder::class);
-        $this->call(DiarySeeder::class);
-        $this->call(DiaryStateSeeder::class);
-        $this->call(SideTypeSeeder::class);
-        $this->call(IncentivesTypeSeeder::class);
         $this->call(JobTypeSeeder::class);
         $this->call(DirectorateSeeder::class);
-        $this->call(ReminderNotificationSeeder::class);
-        $this->call(RequirementsTypeSeeder::class);
-        $this->call(TaskDegreeSeeder::class);
-        $this->call(TaskStateSeeder::class);
-        $this->call(TasksTypeSeeder::class);
-        $this->call(WaqfTypeSeeder::class);
         $this->call(SessionTypeSeeder::class);
         $this->call(CorrectAnswerSeeder::class);
-        $this->call(DepartmentTypeSeeder::class);
-        $this->call(MyeventSeeder::class);
         $this->call(SubmitMethodSeeder::class);
         $this->call(OpportunityTypeSeeder::class);
         $this->call(QuestionTypeSeeder::class);
